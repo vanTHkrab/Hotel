40743734430b21d7270ac68106a4b685bd6a4ebd
vanTHkrab <vanth2548@gmail.com>
2024-08-17 14:57:35 +0700

    package

--- style2/js/main.js
+++ style2/js/main.js
@@ -3,11 +3,13 @@
 
     // Spinner
     var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
+        $('#spinner').removeClass('show');
+        // setTimeout(function () {
+        //     if ($('#spinner').length > 0) {
+        //         $('#spinner').removeClass('show');
+        //     }
+        // }, 1);
+        
     };
     spinner();
     
