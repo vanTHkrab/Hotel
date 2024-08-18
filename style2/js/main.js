4cd442866df9efbdfdeb22997ca91d2b99e93298
vanTHkrab <vanth2548@gmail.com>
2024-08-18 13:53:10 +0700

    test

--- style2/js/main.js
+++ style2/js/main.js
@@ -4,11 +4,11 @@
     // Spinner
     var spinner = function () {
         $('#spinner').removeClass('show');
-        // setTimeout(function () {
-        //     if ($('#spinner').length > 0) {
-        //         $('#spinner').removeClass('show');
-        //     }
-        // }, 1);
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
         
     };
     spinner();
